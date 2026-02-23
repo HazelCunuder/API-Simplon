b36cf221a95943af0f2227ef0ee25dba9513702e
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-02-23 13:16:44 +0100

    refactor(src/model/user.py): Change role type and remove extra comma

--- src/model/user.py
+++ src/model/user.py
@@ -9,7 +9,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     last_name = Column(String(50), nullable=False)
-    first_name = Column(String(50), nullable=False, )
+    first_name = Column(String(50), nullable=False)
     email = Column(String, unique=True, nullable=False)
-    role = Column(String, nullable=False, default="User")
+    role = Column(Integer, nullable=False, default=2)
     register_date = Column(Date, nullable=False)
