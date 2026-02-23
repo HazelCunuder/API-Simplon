84fde5f6c0b0c24b36b84b9cd5753cf7a34c0c28
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-02-23 11:51:29 +0100

    feat(src/model): add Courses model

---
+++ src/model/courses.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Courses(Base):
+
+    __tablename__ = "courses_table"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50), nullable=False)
+    description = Column(Text, nullable=True)
+    duration = Column(Integer, nullable=False)
+    level = Column(String)
