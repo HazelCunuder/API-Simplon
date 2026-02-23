f7d694482d188aa9b8cf973ddd3622af702b4c9a
NICHIKU <puypeethan@gmail.com>
2026-02-23 16:42:28 +0100

    Feat(KAN-15) : Fiexed the rest of the DTOs

--- src/schemas/user_schema.py
+++ src/schemas/user_schema.py
@@ -3,21 +3,26 @@ from datetime import date
 
     
 class UserCreate(BaseModel):
-    last_name: str = Field(..., description="The user's last name", strict=True)
-    first_name: str = Field(..., description="The user's first name", strict=True)
-    email: EmailStr = Field(..., description="The user's email address", strict=True)
-    role: int = Field(..., description="The user's role (0=Admin, 1=Manager, 2=Employee)", ge=0, le=2)
-    register_date: date = Field(..., description="The date the user registered")
+    last_name: str = Field(description="The user's last name", strict=True)
+    first_name: str = Field(description="The user's first name", strict=True)
+    email: EmailStr = Field(description="The user's email address", strict=True)
+    role: int = Field(description="The user's role (0=Admin, 1=Manager, 2=Employee)", ge=0, le=2)
+    register_date: date = Field(description="The date the user registered")
 
 class UserUpdate(BaseModel):
-    last_name: str | None
-    first_name: str | None
-    email: EmailStr | None
-    role: int | None
-    register_date: date | None
+    last_name: str = Field(description="The user's last name", strict=True)
+    first_name: str = Field(description="The user's first name", strict=True)
+    email: EmailStr = Field(description="The user's email address", strict=True)
+    role: int = Field(description="The user's role (0=Admin, 1=Manager, 2=Employee)", ge=0, le=2)
+    register_date: date = Field(description="The date the user registered")
 
 class UserDelete(BaseModel):
-    id: int
+    id: int = Field(description="The user's ID", ge=0)
+    last_name: str = Field(description="The user's last name", strict=True)
+    first_name: str = Field(description="The user's first name", strict=True)
+    email: EmailStr = Field(description="The user's email address", strict=True)
+    role: int = Field(description="The user's role (0=Admin, 1=Manager, 2=Employee)", ge=0, le=2)
+    register_date: date = Field(description="The date the user registered")
     
 
 @validate_call
