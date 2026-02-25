281f19281263cff0588f20372cc45208e5af9d82
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-02-25 09:29:17 +0100

    feat: add courses router to the api file

--- src/api/v1/routers/api.py
+++ src/api/v1/routers/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from api.v1.endpoints.user_endpoints import router as user_router
+from api.v1.endpoints.courses_endpoints import router as courses_router
 
 api_router = APIRouter()
 api_router.include_router(user_router)
+api_router.include_router(courses_router)
