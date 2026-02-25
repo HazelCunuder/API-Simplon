f186cb159429e26ec9ee2bf67a9279cd747fcbea
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-02-25 10:12:59 +0100

    feat: add correct routes to each post/put/delete/get in courses

--- src/api/v1/endpoints/courses_endpoints.py
+++ src/api/v1/endpoints/courses_endpoints.py
@@ -13,24 +13,24 @@ async def get_courses(course: ShowCoursesSchema):
 async def get_course(course_id: int, course: ShowCoursesSchema):
     return {"message": f"Get course with id {course_id}"}
 
-@router.get("/{course_id}/simple")
+@router.get("/simple/{course_id}")
 async def get_simple_course_info(course_id: int, course: ShowSimpleCourseInfoSchema):
     return {"message": f"Get simple info for course with id {course_id}"}
 
 # POST
 
-@router.post("/")
+@router.post("/create-course")
 async def create_course(course: CoursesCreateSchema):
     return {"message": "Create a new course", "course": course}
 
 # PUT
 
-@router.put("/{course_id}")
+@router.put("/update/{course_id}")
 async def update_course(course_id: int, course: ModifyCoursesSchema):
     return {"message": f"Update course with id {course_id}", "course": course}
 
 # DELETE
 
-@router.delete("/{course_id}")
+@router.delete("/delete/{course_id}")
 async def delete_course(course_id: int):
     return {"message": f"Delete course with id {course_id}"}
