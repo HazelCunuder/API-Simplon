1e4f227472e64ea6d1bb291fe0ea58a172788564
NICHIKU <puypeethan@gmail.com>
2026-02-25 09:36:06 +0100

    fix(sessions) : fixed the import in sessions endpoints

--- src/api/v1/endpoints/session_endpoints.py
+++ src/api/v1/endpoints/session_endpoints.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status
-from schemas.sessions_schema import SessionCreate
+from schemas.sessions_schema import SessionCreate, SessionRead, SessionUpdate
 
 router = APIRouter(prefix="/sessions", tags=["sessions"])
 
@@ -9,11 +9,18 @@ async def create_session(session: SessionCreate):
     return {"message": "Session created", "data": session}
 
 @router.put("/sessions/{session_id}")
-async def update_session(session_id: int, session: SessionCreate):
+async def update_session(session_id: int, session: SessionUpdate):
     # TODO : call service
     return {"message": f"Session {session_id} updated"}
 
-@router.delete("/sessions/{session_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_session(session_id: int):
+@router.get("/sessions/{session_id}", response_model=SessionRead)
+async def get_session(session_id: int):
     # TODO : call service
-    return None
+    return {"message": f"Session {session_id} details", "data": SessionRead(
+        id=session_id,
+        teacher_id=1,
+        course_id=1,
+        start_date="2024-01-01",
+        end_date="2024-01-10",
+        capacity=30
+    )}
