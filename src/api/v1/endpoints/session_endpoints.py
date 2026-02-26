55aebeae76313e17aeee4a0942b1a7dbe6533d09
Ethan Puype <puypeethan@gmail.com>
2026-02-26 14:38:34 +0100

    feat(session) : added security login to the sessions

--- src/api/v1/endpoints/session_endpoints.py
+++ src/api/v1/endpoints/session_endpoints.py
@@ -1,22 +1,26 @@
 from fastapi import APIRouter, status, Depends
 from schemas.sessions_schema import SessionCreate, SessionRead, SessionUpdate
 from services.session_services import SessionService
-
+from utils.security import verify_token
 
 router = APIRouter(prefix="/sessions", tags=["sessions"])
 
 @router.get("/{session_id}", response_model=SessionRead)
 async def get_session(session_id: int, service: SessionService = Depends()):
+    _: dict = Depends(verify_token)
     return service.get_session(session_id)
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_session(session: SessionCreate, service: SessionService = Depends()):
+    _: dict = Depends(verify_token)
     return service.create_session(session)
 
 @router.put("/{session_id}")
 async def update_session(session_id: int, session: SessionUpdate, service: SessionService = Depends()):
+    _: dict = Depends(verify_token)
     return service.update_session(session_id, session)  
 
 @router.get("/{session_id}", response_model=SessionRead)
 async def get_session(session_id: int, service: SessionService = Depends()):
+    _: dict = Depends(verify_token)
     return service.get_session(session_id)
