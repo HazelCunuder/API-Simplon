f7e716c7cac2ee3079d5526381100a6248439cc5
Umberto Emonds <umberto.emonds@gmail.com>
2026-02-26 13:53:35 +0100

    feat(auth): init auth and security

--- src/api/v1/endpoints/user_endpoints.py
+++ src/api/v1/endpoints/user_endpoints.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, status, Depends
 from schemas.user_schema import UserCreate, UserUpdate
 from services.user_service import UserService
+from utils.security import verify_token
 
 router = APIRouter(prefix="/user", tags=["user"])
 
 @router.get("/{user_id}", status_code=status.HTTP_200_OK)
 async def get_user(
     user_id: int,
+    _: dict = Depends(verify_token),
     service: UserService = Depends()
 ):
     return service.get_user(user_id)
@@ -22,6 +24,7 @@ async def create_user(
 async def update_user(
     user_id: int,
     user: UserUpdate,
+    _: dict = Depends(verify_token),
     service: UserService = Depends()
 ):
     return service.update_user(user_id, user)
@@ -29,6 +32,7 @@ async def update_user(
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_user(
     user_id: int,
+    _: dict = Depends(verify_token),
     service: UserService = Depends()
 ):
     return service.delete_user(user_id)
--- src/utils/security.py
+++ src/utils/security.py
@@ -1,5 +1,61 @@
+from datetime import timedelta, datetime, timezone
 from argon2 import PasswordHasher
-from argon2.exceptions import VerifyMismatchError
+from argon2.exceptions import VerifyMismatchError, InvalidHashError
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from fastapi import status
+
+# -- Token -- #
+
+SECRET_KEY = "4f9s8d7f6sdf98s7df9s8df7s9df8s7df98sdf7"
+ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="login",
+    description="JWT Bearer token"
+)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return encoded_jwt
+
+
+async def verify_token(token: str = Depends(oauth2_scheme)) -> dict:
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id: str = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token",
+            )
+
+        return payload
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
+
+# -- Password -- #
 
 ph = PasswordHasher(
     time_cost=3,        # iterations
@@ -10,8 +66,10 @@ ph = PasswordHasher(
 def hash_password(password: str) -> str:
     return ph.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+def verify_password(hashed_password: str, plain_password: str) -> bool:
     try:
         return ph.verify(hashed_password, plain_password)
     except VerifyMismatchError:
+        return False
+    except InvalidHashError:
         return False
