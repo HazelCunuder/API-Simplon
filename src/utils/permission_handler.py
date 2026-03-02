0ef0ba2bdc679b82e5e7a84f55cfcccce119c4d2
Umberto Emonds <umberto.emonds@gmail.com>
2026-03-02 15:42:58 +0100

    fix(permissions): role checker

--- src/utils/permission_handler.py
+++ src/utils/permission_handler.py
@@ -4,7 +4,15 @@ from utils.security import get_logged_user
 
 def role_checker(required_role: Role):
     async def check_role(user = Depends(get_logged_user)):
-        if user.role != required_role:
+        allowed_roles = [Role.STUDENT]
+
+        match user.role:
+            case Role.TEACHER:
+                allowed_roles = [Role.STUDENT, Role.TEACHER]
+            case Role.ADMIN:
+                allowed_roles = [Role.STUDENT, Role.TEACHER, Role.ADMIN]
+
+        if not required_role in allowed_roles:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Not enough permissions"
