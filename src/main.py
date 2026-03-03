741acb86ab735c72763fea8613a67ab08f207162
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-03-03 09:16:38 +0100

    feat: add CORS middleware

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from api.v1.routers.api import api_router
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from utils.exceptions import (
     AppError,
@@ -24,9 +25,19 @@ from utils.exceptions import (
 
 app = FastAPI()
 
-@app.get('/')
-def welcome():
-    return {'message': 'Welcome to my FastAPI application'}
+origins = [
+    "http://localhost",
+    "http://localhost:8080"
+    # Insert origin for frontend here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    )
 
 app.include_router(api_router)
 
